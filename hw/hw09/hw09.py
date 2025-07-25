96f987040417502b21f7ce2d2df3e766cf01346e
Eric Kim <erickim555@gmail.com>
2025-07-24 23:42:38 -0700

    Deploy website 07/24/2025 23:42:38

--- hw/hw09/hw09.py
+++ hw/hw09/hw09.py
@@ -9,6 +9,71 @@ def label_squarer(t):
     "*** YOUR CODE HERE ***"
 
 
+def add_d_leaves(t, v):
+    """Add d leaves containing v to each node at every depth d.
+
+    >>> t_one_to_four = Tree(1, [Tree(2), Tree(3, [Tree(4)])])
+    >>> print(t_one_to_four)
+    1
+      2
+      3
+        4
+    >>> add_d_leaves(t_one_to_four, 5)
+    >>> print(t_one_to_four)
+    1
+      2
+        5
+      3
+        4
+          5
+          5
+        5
+
+    >>> t0 = Tree(9)
+    >>> add_d_leaves(t0, 4)
+    >>> t0
+    Tree(9)
+    >>> t1 = Tree(1, [Tree(3)])
+    >>> add_d_leaves(t1, 4)
+    >>> t1
+    Tree(1, [Tree(3, [Tree(4)])])
+    >>> t2 = Tree(2, [Tree(5), Tree(6)])
+    >>> t3 = Tree(3, [t1, Tree(0), t2])
+    >>> print(t3)
+    3
+      1
+        3
+          4
+      0
+      2
+        5
+        6
+    >>> add_d_leaves(t3, 10)
+    >>> print(t3)
+    3
+      1
+        3
+          4
+            10
+            10
+            10
+          10
+          10
+        10
+      0
+        10
+      2
+        5
+          10
+          10
+        6
+          10
+          10
+        10
+    """
+    "*** YOUR CODE HERE ***"
+
+
 class Tree:
     """A tree has a label and a list of branches.
 
