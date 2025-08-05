2dee4823327244e966d055b829264c86e97c0859
Eric Kim <erickim555@gmail.com>
2025-08-04 17:22:32 -0700

    Deploy website 08/04/2025 17:22:31

---
+++ hw/sol-hw08/hw08.py
@@ -0,0 +1,272 @@
+# You do not need to understand what this function does. It is used for testing.
+def make_test_random():
+    """A deterministic random function that cycles between
+    [0.0, 0.1, 0.2, ..., 0.9] for testing purposes.
+
+    >>> random = make_test_random()
+    >>> random()
+    0.0
+    >>> random()
+    0.1
+    >>> random2 = make_test_random()
+    >>> random2()
+    0.0
+    """
+    rands = [x / 10 for x in range(10)]
+    def random():
+        rand = rands[0]
+        rands.append(rands.pop(0))
+        return rand
+    return random
+
+### Phase 1: The Player Class
+class Player:
+    """
+    >>> random = make_test_random()
+    >>> p1 = Player('Hill', random)
+    >>> p2 = Player('Don', random)
+    >>> p1.popularity
+    100
+    >>> p1.debate(p2)  # random() should return 0.0
+    >>> p1.popularity
+    150
+    >>> p2.popularity
+    100
+    >>> p2.votes
+    0
+    >>> p2.speech(p1)
+    >>> p2.votes
+    10
+    >>> p2.popularity
+    110
+    >>> p1.popularity
+    135
+    >>> p1.speech(p2)
+    >>> p1.votes
+    13
+    >>> p1.popularity
+    148
+    >>> p2.votes
+    10
+    >>> p2.popularity
+    99
+    >>> for _ in range(4):  # 0.1, 0.2, 0.3, 0.4
+    ...     p1.debate(p2)
+    >>> p2.debate(p1)
+    >>> p2.popularity
+    49
+    >>> p2.debate(p1)
+    >>> p2.popularity
+    0
+    """
+    def __init__(self, name, random_func):
+        self.name = name
+        self.votes = 0
+        self.popularity = 100
+        self.random_func = random_func
+
+    def debate(self, other):
+        prob = max(0.1, self.popularity / (self.popularity + other.popularity))
+        if self.random_func() < prob:
+            self.popularity += 50
+        else:
+            self.popularity = max(0, self.popularity - 50)
+
+    def speech(self, other):
+        self.votes += self.popularity // 10
+        self.popularity += self.popularity // 10
+        other.popularity = max(0, other.popularity - (other.popularity // 10))
+
+    def choose(self, other):
+        return self.speech
+
+
+### Phase 2: The Game Class
+class Game:
+    """
+    >>> random = make_test_random()
+    >>> p1, p2 = Player('Hill',random), Player('Don', random)
+    >>> g = Game(p1, p2)
+    >>> winner = g.play()
+    >>> p1 is winner
+    True
+    >>> # Additional correctness tests
+    >>> winner is g.winner()
+    True
+    >>> g.turn
+    10
+    >>> p1.votes = p2.votes
+    >>> print(g.winner())
+    None
+    """
+    def __init__(self, player1, player2):
+        self.p1 = player1
+        self.p2 = player2
+        self.turn = 0
+
+    def play(self):
+        while not self.game_over():
+            if self.turn % 2 == 0:
+                curr, other = self.p1, self.p2
+            else:
+                curr, other = self.p2, self.p1
+            curr.choose(other)(other)
+            self.turn += 1
+        return self.winner()
+
+    def game_over(self):
+        return max(self.p1.votes, self.p2.votes) >= 50 or self.turn >= 10
+
+    def winner(self):
+        if self.p1.votes > self.p2.votes:
+            return self.p1
+        elif self.p2.votes > self.p1.votes:
+            return self.p2
+        else:
+            return None
+
+
+### Phase 3: New Players
+class AggressivePlayer(Player):
+    """
+    >>> random = make_test_random()
+    >>> p1, p2 = AggressivePlayer('Don', random), Player('Hill', random)
+    >>> g = Game(p1, p2)
+    >>> winner = g.play()
+    >>> p1 is winner
+    True
+    >>> # Additional correctness tests
+    >>> p1.popularity = p2.popularity
+    >>> p1.choose(p2) == p1.debate
+    True
+    >>> p1.popularity += 1
+    >>> p1.choose(p2) == p1.debate
+    False
+    >>> p2.choose(p1) == p2.speech
+    True
+    """
+    def choose(self, other):
+        if self.popularity <= other.popularity:
+            return self.debate
+        else:
+            return self.speech
+
+class CautiousPlayer(Player):
+    """
+    >>> random = make_test_random()
+    >>> p1, p2 = CautiousPlayer('Hill', random), AggressivePlayer('Don', random)
+    >>> p1.popularity = 0
+    >>> p1.choose(p2) == p1.debate
+    True
+    >>> p1.popularity = 1
+    >>> p1.choose(p2) == p1.debate
+    False
+    >>> # Additional correctness tests
+    >>> p2.choose(p1) == p2.speech
+    True
+    """
+    def choose(self, other):
+        if self.popularity == 0:
+            return self.debate
+        else:
+            return self.speech
+
+
+def store_digits(n):
+    """Stores the digits of a positive number n in a linked list.
+
+    >>> s = store_digits(1)
+    >>> s
+    Link(1)
+    >>> store_digits(2345)
+    Link(2, Link(3, Link(4, Link(5))))
+    >>> store_digits(876)
+    Link(8, Link(7, Link(6)))
+    >>> store_digits(2450)
+    Link(2, Link(4, Link(5, Link(0))))
+    >>> store_digits(20105)
+    Link(2, Link(0, Link(1, Link(0, Link(5)))))
+    >>> # a check for restricted functions
+    >>> import inspect, re
+    >>> cleaned = re.sub(r"#.*\\n", '', re.sub(r'"{3}[\s\S]*?"{3}', '', inspect.getsource(store_digits)))
+    >>> print("Do not use str or reversed!") if any([r in cleaned for r in ["str", "reversed"]]) else None
+    """
+    result = Link.empty
+    while n > 0:
+        result = Link(n % 10, result)
+        n //= 10
+    return result
+
+
+def deep_map_mut(func, s):
+    """Mutates a deep link s by replacing each item found with the
+    result of calling func on the item. Does NOT create new Links (so
+    no use of Link's constructor).
+
+    Does not return the modified Link object.
+
+    >>> link1 = Link(3, Link(Link(4), Link(5, Link(6))))
+    >>> print(link1)
+    <3 <4> 5 6>
+    >>> # Disallow the use of making new Links before calling deep_map_mut
+    >>> Link.__init__, hold = lambda *args: print("Do not create any new Links."), Link.__init__
+    >>> try:
+    ...     deep_map_mut(lambda x: x * x, link1)
+    ... finally:
+    ...     Link.__init__ = hold
+    >>> print(link1)
+    <9 <16> 25 36>
+    """
+    if s is Link.empty:
+        return None
+    elif isinstance(s.first, Link):
+        deep_map_mut(func, s.first)
+    else:
+        s.first = func(s.first)
+    deep_map_mut(func, s.rest)
+
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+from reprlib import recursive_repr
+Link.__repr__ = recursive_repr()(Link.__repr__)
+
